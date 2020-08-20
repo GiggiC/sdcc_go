121e05b89c7aca36d5c89ccea34fd14b4c113ccd
luigi <l.corsi@hotmail.it>
2020-08-20 22:33:00 +0200

    Initial commit luigi-branch

--- websockets.go
+++ websockets.go
@@ -4,7 +4,10 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/websocket"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
 var upgrader = websocket.Upgrader{
@@ -18,7 +21,12 @@ type Object struct {
 }
 
 func main() {
-	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", serveTemplate)
+
+	http.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request) {
 		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
 
 		for {
@@ -38,9 +46,20 @@ func main() {
 		}
 	})
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "websockets.html")
-	})
+	log.Println("Listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	http.ListenAndServe(":8080", nil)
+
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	lp := filepath.Join("templates", "layout.html")
+	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+
+	tmpl, _ := template.ParseFiles(lp, fp)
+	tmpl.ExecuteTemplate(w, "layout", nil)
 }
