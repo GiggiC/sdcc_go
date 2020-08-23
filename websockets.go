c6940c4e3b0d2e2a0a96a25202f60cc9e27562a5
luigi <l.corsi@hotmail.it>
2020-08-24 00:07:36 +0200

    Front end

--- websockets.go
+++ websockets.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/gomodule/redigo/redis"
 	_ "github.com/gomodule/redigo/redis"
+	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 	_ "github.com/lib/pq"
 	"golang.org/x/crypto/bcrypt"
@@ -98,12 +99,24 @@ func publisTo(topic string, data string) {
 
 func initCache() {
 	// Initialize the redis connection to a redis instance running on your local machine
-	conn, err := redis.DialURL("redis://localhost")
+	conn, err := redis.DialURL("redis://localhost:6379")
 	if err != nil {
 		panic(err)
 	}
 	// Assign the connection to the package level `cache` variable
 	cache = conn
+}
+
+func (s *server) notifications(res http.ResponseWriter, req *http.Request) {
+
+	if req.Method != "POST" {
+		lp := filepath.Join("templates", "layout.html")
+		fp := filepath.Join("templates", "notifications.html")
+
+		tmpl, _ := template.ParseFiles(lp, fp)
+		tmpl.ExecuteTemplate(res, "layout", nil)
+		return
+	}
 }
 
 func (s *server) signupPage(res http.ResponseWriter, req *http.Request) {
@@ -164,6 +177,38 @@ func (s *server) signupPage(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (s *server) publish(res http.ResponseWriter, req *http.Request) {
+
+	// We can obtain the session token from the requests cookies, which come with every request
+	c, err := req.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			// If the cookie is not set, return an unauthorized status
+			res.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		// For any other type of error, return a bad request status
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	sessionToken := c.Value
+
+	// We then get the name of the user from our cache, where we set the session token
+	response, err := cache.Do("GET", sessionToken)
+	if err != nil {
+		// If there is an error fetching from cache, return an internal server error status
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if response == nil {
+		// If the session token is not present in cache, return an unauthorized error
+		res.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	// Finally, return the welcome message to the user
+	res.Write([]byte(fmt.Sprintf("Welcomeeeeeee %s!", response)))
+}
+
 func (s *server) index(res http.ResponseWriter, req *http.Request) {
 
 	if req.Method != "POST" {
@@ -194,9 +239,114 @@ func (s *server) index(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	res.Write([]byte("Hello" + databaseEmail))
+	// Create a new random session token
+	sessionToken := uuid.New().String()
+
+	// Set the token in the cache, along with the user whom it represents
+	// The token has an expiry time of 120 seconds
+	_, err = cache.Do("SETEX", sessionToken, "120", databaseEmail)
+	if err != nil {
+		// If there is an error in setting the cache, return an internal server error
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Finally, we set the client cookie for "session_token" as the session token we just generated
+	// we also set an expiry time of 120 seconds, the same as the cache
+	http.SetCookie(res, &http.Cookie{
+		Name:    "session_token",
+		Value:   sessionToken,
+		Expires: time.Now().Add(120 * time.Second),
+	})
+
+	http.Redirect(res, req, "/notifications", 301)
 
 	return
+}
+
+func Refresh(res http.ResponseWriter, req *http.Request) {
+	// (BEGIN) The code uptil this point is the same as the first part of the `Welcome` route
+	c, err := req.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			res.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	sessionToken := c.Value
+
+	response, err := cache.Do("GET", sessionToken)
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if response == nil {
+		res.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	// (END) The code uptil this point is the same as the first part of the `Welcome` route
+
+	// Now, create a new session token for the current user
+	newSessionToken := uuid.New().String()
+	_, err = cache.Do("SETEX", newSessionToken, "120", fmt.Sprintf("%s", response))
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Delete the older session token
+	_, err = cache.Do("DEL", sessionToken)
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Set the new token as the users `session_token` cookie
+	http.SetCookie(res, &http.Cookie{
+		Name:    "session_token",
+		Value:   newSessionToken,
+		Expires: time.Now().Add(120 * time.Second),
+	})
+
+	http.Redirect(res, req, "/notifications", 301)
+
+}
+
+func logout(res http.ResponseWriter, req *http.Request) {
+
+	// (BEGIN) The code uptil this point is the same as the first part of the `Welcome` route
+	c, err := req.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			res.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	sessionToken := c.Value
+
+	response, err := cache.Do("GET", sessionToken)
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if response == nil {
+		res.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	// (END) The code uptil this point is the same as the first part of the `Welcome` route
+
+	// Set the new token as the users `session_token` cookie
+	http.SetCookie(res, &http.Cookie{
+		Name:    sessionToken,
+		Value:   "",
+		Expires: time.Now().Add(0 * time.Second),
+	})
+
+	http.Redirect(res, req, "/notifications", 301)
 }
 
 func main() {
@@ -228,8 +378,12 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/", s.index)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/refresh", Refresh)
 	http.HandleFunc("/registration", s.signupPage)
 	http.HandleFunc("/registrationError", registrationError)
+	http.HandleFunc("/publish", s.publish)
+	http.HandleFunc("/notifications", s.notifications)
 
 	/*http.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request) {
 		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
