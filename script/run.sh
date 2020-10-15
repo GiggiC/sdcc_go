cfedeb59b714119c3c50af4c5fdaf686c7229095
luigi <l.corsi@hotmail.it>
2020-10-15 12:13:24 +0200

    Testing

--- script/run.sh
+++ script/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/go/src/github.com/GiggiC/sdcc_go/src
-(redis-server &) && (go run /home/ec2-user/go/src/github.com/GiggiC/sdcc_go/src/*.go)
+(redis-server &) && (go run *.go)
