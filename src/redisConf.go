a571cbee310a235055619c1166044bc73f851abd
luigi <l.corsi@hotmail.it>
2020-10-31 14:54:21 +0100

    Comments

--- src/redisConf.go
+++ src/redisConf.go
@@ -15,8 +15,8 @@ func initRedis() {
 	dsn := os.Getenv("REDIS_DSN")
 
 	if len(dsn) == 0 {
-		//dsn = "localhost:6379" //localhost configuration
-		dsn = "18.211.56.181:6379" //EC2 configuration
+		dsn = "localhost:6379" //localhost configuration
+		//dsn = "18.211.56.181:6379" //EC2 configuration
 	}
 
 	client = redis.NewClient(&redis.Options{
